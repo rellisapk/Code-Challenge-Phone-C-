5d50f3e5bf8bd333cb2a2d1425953cf37f88342a
rellisapk <74693812+rellisapk@users.noreply.github.com>
2025-03-14 11:38:56 +0700

    Update test_old_phone_pad.cpp

--- test_old_phone_pad.cpp
+++ test_old_phone_pad.cpp
@@ -2,32 +2,25 @@
 #include <cassert>
 #include "old_phone_pad.h"
 
-using namespace std;
-
 void testOldPhonePad() {
-    // Test 1
     assert(OldPhonePad("33#") == "E");
-    cout << "Test 1 Passed!\n";
+    std::cout << "Test 1: Passed ✅\n";
 
-    // Test 2
     assert(OldPhonePad("227#") == "B");
-    cout << "Test 2 Passed!\n";
+    std::cout << "Test 2: Passed ✅\n";
 
-    // Test 3
     assert(OldPhonePad("4433555 555666#") == "HELLO");
-    cout << "Test 3 Passed!\n";
+    std::cout << "Test 3: Passed ✅\n";
 
-    // Test 4:
     assert(OldPhonePad("8 88777444666*664#") == "TURNG");
-    cout << "Test 4 Passed!\n";
+    std::cout << "Test 4: Passed ✅\n";
 
-    // Test 5
     assert(OldPhonePad("#") == "");
-    cout << "Test 5 Passed!\n";
+    std::cout << "Test 5: Passed ✅\n";
 }
 
 int main() {
     testOldPhonePad();
-    cout << "Semua tes berhasil!\n";
+    std::cout << "All test has passed! 🎉\n";
     return 0;
 }
